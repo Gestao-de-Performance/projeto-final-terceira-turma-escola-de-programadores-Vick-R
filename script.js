60ce57614976d7a4b0ec8989605caeb1383aba03
Vick-R <123656856+Vick-R@users.noreply.github.com>
2024-09-01 19:38:56 -0300

    att

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ function toggleMenu() {
         nav.style.display = 'none';
     }
 }
+
+document.getElementById('downloadButton').addEventListener('click', function() {
+    const pdfUrl = 'pdf/curriculo.pdf'; 
+    const link = document.createElement('a'); 
+    link.href = pdfUrl; 
+    link.download = 'curriculo.pdf'; 
+    document.body.appendChild(link); 
+    link.click(); 
+    document.body.removeChild(link); 
+});
